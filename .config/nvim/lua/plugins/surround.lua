7fcaec2e6d9d783ee1ca5797aec04f8619de6f90
Sean M. Duncan <sean.m.duncan@medtronic.com>
2025-11-13 15:10:07 -0800

    feat: add and setup mini.surround

---
+++ .config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          keys = { "f", "F", "t", "T" },
+        },
+      },
+    },
+  },
+  {
+    "mini-nvim/mini.surround",
+    opts = {
+      mappings = {
+        add = ";;",
+        delete = ";d",
+        find = ";f",
+        find_left = ";F",
+        highlight = ";h",
+        replace = ";r",
+        update_n_lines = ";n",
+      },
+    },
+  },
+}
